975ecfdbd33e57c0718a128b0caee954d3388f67
Jucian0 <juciano@outlook.com.br>
2022-05-16 21:44:43 -0300

    relacionais

---
+++ fundamentos/relacionais/relacionais.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Strings", "Banana" == "Banana")
+	fmt.Println("!=", 3 != 2)
+	fmt.Println("<", 3 < 2)
+	fmt.Println(">", 3 > 2)
+	fmt.Println("<=", 3 <= 2)
+	fmt.Println(">=", 3 >= 2)
+	fmt.Println("&&", true && true)
+
+	fmt.Println("||", true || false)
+	fmt.Println("!", !true)
+
+	d1 := time.Unix(0, 0)
+	d2 := time.Unix(0, 0)
+
+	fmt.Println("Datas", d1 == d2)
+	fmt.Println("Datas", d1 != d2)
+	fmt.Println("Datas", d1.Equal(d2))
+
+	type Pessoa {
+		Nome string
+	}
+
+	p1 := Pessoa{"Nome 1"}
+	p2 := Pessoa{"Nome 2"}
+
+	fmt.Println("Pessoas", p1 == p2)
+}
