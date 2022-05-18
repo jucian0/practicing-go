b857915be4e70d08b92fb50d2345808f02ccaa45
Jucian0 <juciano@outlook.com.br>
2022-05-17 22:25:09 -0300

    switch

--- estrutura_de_controle/switch/switch.go
+++ estrutura_de_controle/switch/switch.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func notaParaLetra(nota float64) string {
 	switch nota {
 	case 10:
@@ -11,12 +16,38 @@ func notaParaLetra(nota float64) string {
 	case 8.5:
 		return "B"
 	case 8:
-
+		return "B-"
 	default:
 		return "C"
+	}
+}
+
+func switch2() {
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Bom dia")
+	case t.Hour() < 18:
+		fmt.Println("Boa tarde")
+	default:
+		fmt.Println("Boa noite")
 
 	}
+}
 
+func switch3(i interface{}) string {
+
+	switch i.(type) {
+	case int:
+		return "int"
+	case string:
+		return "string"
+	case float64:
+		return "float64"
+	default:
+		return "outro"
+	}
 }
 
 func main() {
