1f475fd8341effafc01ee6ae6d6e496f8c3ad7c1
Jucian0 <juciano@outlook.com.br>
2022-05-19 22:16:26 -0300

    slice 2

--- array_slice_map/slice/slice.go
+++ array_slice_map/slice/slice.go
@@ -12,4 +12,14 @@ func main() {
 
 	fmt.Println(a1, s1)
 	fmt.Println(reflect.TypeOf(a1), reflect.TypeOf(s1))
+
+	a2 := [5]int{1, 2, 3, 4, 5} // array
+	s2 := a2[1:3]               // slice
+
+	fmt.Println(a2, s2)
+	fmt.Println(reflect.TypeOf(a2), reflect.TypeOf(s2))
+
+	s3 := a2[:2] // slice
+
+	fmt.Println(a2, s3)
 }
